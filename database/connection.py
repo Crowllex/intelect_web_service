87ca23f8dd1d1fb208931ce6dc2ca14cce341722
Roberto <irobertuber@gmail.com>
2021-11-17 19:10:32 -0500

    se arreglo la conexion a la bd con las variables de entorno

--- database/connection.py
+++ database/connection.py
@@ -7,8 +7,8 @@ import os
 
 class Connection():
     def __init__(self) -> None:
-        self.db_link = dbc.connect(host=os.environ.DB_HOST, user=os.environ.DB_USER, passwd=os.environ.DB_PASSWORD,
-                                   db=os.environ.DB_NAME, port=os.environ.DB_PORT, cursorclass=cursors.DictCursor)
+        self.db_link = dbc.connect(host=os.getenv('DB_HOST'), user=os.getenv('DB_USER'), passwd=os.getenv('DB_PASSWORD'),
+                                   db=os.getenv('DB_NAME'), port=int(os.getenv('DB_PORT')), cursorclass=cursors.DictCursor)
 
     @property
     def open(self):
