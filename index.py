575a04958f45358076965f8d6abb0961c195d5fa
carlos5897 <83098277+carlos5897@users.noreply.github.com>
2021-11-19 21:09:13 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from controllers.usuario import route_usuario
 from controllers.sesion import route_sesion
+from controllers.sede import route_sede
+from controllers.rubro import route_rubro
+from controllers.comprobantes import route_comprobante
 import pymysql
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.register_blueprint(route_usuario)
 app.register_blueprint(route_sesion)
+app.register_blueprint(route_sede)
+app.register_blueprint(route_rubro)
+app.register_blueprint(route_comprobante)
 
 
 @app.route('/welcome')
