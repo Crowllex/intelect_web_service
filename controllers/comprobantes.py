281fabf48c38bb5e50650191f4fa487482c77229
Carlos <cedb8597@gmail.com>
2021-11-19 23:31:34 -0500

    Se quitaron los comentarios y se arreglaron las rutas

--- controllers/comprobantes.py
+++ controllers/comprobantes.py
@@ -1,17 +1,19 @@
-#Importar los paquetes y clases que serán necesarios para implementar
+
 from flask import Blueprint, app, request, jsonify
 from models.comprobantes import Comprobante
 import json
-import middlewares.validar_token
+from middlewares.validar_token import validate_token
+
 
 route_comprobante = Blueprint('route_comprobante', __name__)
 
 @route_comprobante.route('/comprobante/listar', methods=['POST'])
-@middlewares.validar_token.validate_token.validar_token #Función que permite validar el token antes de acceder al servicio web 
+@validate_token
 def listar():
     if request.method == 'POST':
+        id_usuario=request.form['id_usuario']
         objProd = Comprobante()
-        rptaJson = objProd.listarComprobante()
-        datos_comprobante = json.loads(rptaJson) #Convertir una cadena JSON a objetos JSON
-        return jsonify(datos_comprobante = json.loads(rptaJson) #Convertir una cadena JSON a objetos JSON
-), 200 #200 -> OK
+        rptaJson = objProd.listarComprobante(id_usuario)
+        datos_comprobante = json.loads(rptaJson) 
+        print(datos_comprobante)
+        return jsonify(datos_comprobante),200
--- controllers/sede.py
+++ controllers/sede.py
@@ -1,17 +1,16 @@
-#Importar los paquetes y clases que serán necesarios para implementar
+
 from flask import Blueprint, app, request, jsonify
 from models.sede import Sede
+from middlewares.validar_token import validate_token
 import json
-import middlewares.validar_token
 
 route_sede = Blueprint('route_sede', __name__)
 
 @route_sede.route('/sede/listar', methods=['POST'])
-@middlewares.validar_token.validate_token.validar_token #Función que permite validar el token antes de acceder al servicio web 
+@validate_token 
 def listar():
     if request.method == 'POST':
         objProd = Sede()
         rptaJson = objProd.listarSede()
-        datos_sede = json.loads(rptaJson) #Convertir una cadena JSON a objetos JSON
-        return jsonify(datos_sede = json.loads(rptaJson) #Convertir una cadena JSON a objetos JSON
-), 200 #200 -> OK
+        datos_sede = json.loads(rptaJson)
+        return jsonify(datos_sede), 200 
