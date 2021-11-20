98da0c5bfd76039a63ef9e9314779519a7e20cbc
Carlos <cedb8597@gmail.com>
2021-11-19 23:30:28 -0500

    Se quitaron los comentarios y modificaron los models

--- models/rubro.py
+++ models/rubro.py
@@ -12,27 +12,20 @@ class Rubro():
         self.id_sede = id_sede
 
     def listarRubro(self):
-        #Abrir conexión a la base de datos
         con = db().open
         
-        #Crear un cursor 
         cursor = con.cursor()
 
-        #Preparar la consulta SQL
         sql = "SELECT R.id_rubro, R.nombre AS descripcion, R.monto, R.calculoxdia, S.nombre AS sede FROM rubro R INNER JOIN sede S ON R.id_sede = S.id_sede"
 
-        #Ejecutar la consulta SQL
         cursor.execute(sql)
 
-        #Capturar la consulta SQL
         datos = cursor.fetchall()
 
-        #Cerrar el cursor y la conexión a la base de datos
         cursor.close()
         con.close()
 
-        #Retornar el resultado
-        if datos: #Se le pregunta si existe registro
+        if datos: 
             return json.dumps({'status': True, 'data': datos}, cls=CustomJsonEncoder)
         else:
             return json.dumps({'status': False, 'data': 'No hay registros'})
--- models/sede.py
+++ models/sede.py
@@ -9,27 +9,20 @@ class Sede():
         self.nombre = nombre
 
     def listarSede(self):
-        #Abrir conexión a la base de datos
         con = db().open
         
-        #Crear un cursor 
         cursor = con.cursor()
 
-        #Preparar la consulta SQL
-        sql = "SELECT S.id_sede, R.Nombre, R.monto, R.calculoxdia, S.nombre AS sede FROM sede S INNER JOIN rubro R ON S.id_sede = R.id_sede"
+        sql = "SELECT * FROM sede"
 
-        #Ejecutar la consulta SQL
         cursor.execute(sql)
 
-        #Capturar la consulta SQL
         datos = cursor.fetchall()
 
-        #Cerrar el cursor y la conexión a la base de datos
         cursor.close()
         con.close()
 
-        #Retornar el resultado
-        if datos: #Se le pregunta si existe registro
+        if datos:
             return json.dumps({'status': True, 'data': datos}, cls=CustomJsonEncoder)
         else:
             return json.dumps({'status': False, 'data': 'No hay registros'})
